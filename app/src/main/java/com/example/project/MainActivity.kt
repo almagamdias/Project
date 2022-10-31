7040a3844c4ffc2491162b83c55eda5915199a8b
0.1.0 <dias.almagambetov01@gmail.com>
2022-10-31 14:07:14 +0600

    0.2.1

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.project
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.project.field.Field
+import com.example.project.player.Player
 
 class MainActivity : AppCompatActivity() {
+    private val p = mutableListOf<Player>();
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val field = Field(2);
         field.createGame();
-
+        field.createDeck();
+        //field.distribution();
         setContentView(R.layout.activity_main)
         val tx: TextView = findViewById(R.id.text)
         tx.setText(field.showDeck());
+        val tx2: TextView = findViewById(R.id.text2)
+        tx2.setText(field.numOfPlayers());
     }
 }
--- app/src/main/java/com/example/project/field/Field.kt
+++ app/src/main/java/com/example/project/field/Field.kt
@@ -10,35 +10,35 @@ class Field(private val numOfPlayers: Int) {
     fun showDeck(): String {
         return deck.toString();
     }
-    fun createGame() {
-        fun createDeck() {
-            for (i in 0 until 9) {
-                for (j in 0 until 4) {
-                    val c = Card(i, j);
-                    deck.add(c);
-                }
+    fun createDeck() {
+        for (i in 0 until 9) {
+            for (j in 0 until 4) {
+                val c = Card(i, j);
+                deck.add(c);
             }
         }
-        for (i in  0 until numOfPlayers) {
+        deck.shuffle();
+    }
+    fun createGame() {
+        for (i in  0 until 2) {
             val px = Player(i);
             p.add(px);
         }
-        fun distribution() {
-            for (i in 0 until 6) {
-                for (j in 0 until numOfPlayers) {
-                    p[j].getCards(deck[i * 2 + j]);
-                    deck.removeAt(i * 2 + j);
-                }
+    }
+    fun numOfPlayers(): String {
+        val x = p.size;
+        return "$x";
+    }
+    fun distribution() {
+        for (i in 0 until 6) {
+            for (j in 0 until numOfPlayers) {
+                p[j].getCards(deck[i * 2 + j]);
+                deck.removeAt(i * 2 + j);
             }
         }
-        createDeck();
-        deck.shuffle();
-        distribution();
     }
-    fun getPlayerCards(): String {
-        var t: String = "";
-        for (i in 0 until numOfPlayers)
-            t += p[i].cardsInHand().toString() + " ";
+    fun getPlayerCards(i: Int): String {
+        val t = p[i].cardsInHand().toString();
         return t;
     }
 }
