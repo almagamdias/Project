77e8209019b575ce776d4f1d018403ea3e167219
0.1.0 <dias.almagambetov01@gmail.com>
2022-11-04 15:07:54 +0600

    0.5.0b5

--- app/src/main/java/com/example/project/field/Field.kt
+++ app/src/main/java/com/example/project/field/Field.kt
@@ -69,12 +69,17 @@ class Field(private val numOfPlayers: Int) {
         p[a].placeCard(i)
     }
     fun defend(a: Int) {
+        var canDefend = false
         for (j in 0 until p[a].handSize()) {
-            if(p[a].cardsInHand(j).getSuit() == f[f.size-1].getSuit()) {
+            if (p[a].cardsInHand(j).getSuit() == f[f.size - 1].getSuit()) {
+                canDefend = true
                 f.add(p[a].cardsInHand(j))
                 p[a].placeCard(j)
                 break
             }
+        }
+        if (!canDefend) {
+            p[a].takeCards(f)
         }
     }
 }
