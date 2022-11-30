754989fbd032e5612d6ff80b568a51b6d02a3e91
0.1.0 <dias.almagambetov01@gmail.com>
2022-11-30 12:09:12 +0600

    1.0ex2.1

--- app/src/main/java/com/example/project/Stats.kt
+++ app/src/main/java/com/example/project/Stats.kt
@@ -5,11 +5,23 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class Stats : ViewModel() {
-    private var name = MutableLiveData(0)
+class Stats(private val state: SavedStateHandle) : ViewModel() {
+    private var _win = MutableLiveData(0)
+    private var _draw = MutableLiveData(0)
+    private var _lose = MutableLiveData(0)
     val win: LiveData<Int>
-        get() = name
-    fun setName() {
-        name.value = name.value?.plus(1)
+        get() = _win
+    val draw: LiveData<Int>
+        get() = _draw
+    val lose: LiveData<Int>
+        get() = _lose
+    fun plusWin() {
+        _win.value = _win.value?.plus(1)
+    }
+    fun plusDraw() {
+        _draw.value = _draw.value?.plus(1)
+    }
+    fun plusLose() {
+        _lose.value = _lose.value?.plus(1)
     }
 }
--- app/src/main/java/com/example/project/Test.kt
+++ app/src/main/java/com/example/project/Test.kt
@@ -20,7 +20,7 @@ import com.example.project.cards.Card
 import com.example.project.databinding.FragmentTestBinding
 
 class Test : Fragment() {
-    private val st: Stats by viewModels()
+    private val st: Stats by viewModels{ SavedStateViewModelFactory()}
     @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,9 +36,20 @@ class Test : Fragment() {
         val c: LinearLayout = bind.findViewById(R.id.clubs)
         deck(c, 3)*/
         binding.lifecycleOwner = this
-        st.setName()
+        st.plusWin()
+        st.plusDraw()
+        st.plusLose()
         st.win.observe(viewLifecycleOwner) { win ->
-            binding.stats.text = win.toString()
+            binding.wins.text = "Wins: $win"
+        }
+        st.draw.observe(viewLifecycleOwner) { draw ->
+            binding.draws.text = "Draws: $draw"
+        }
+        st.lose.observe(viewLifecycleOwner) { lose ->
+            binding.loses.text = "Loses: $lose"
+        }
+        binding.add.setOnClickListener {
+            st.plusWin()
         }
         return binding.root
     }
